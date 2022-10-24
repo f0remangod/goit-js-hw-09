ff80dcaeedb9b9ce85a392a924a8b01677ca06d8
f0remangod <106250265+f0remangod@users.noreply.github.com>
2022-10-24 12:34:32 +0000

    Deploying to gh-pages from @ f0remangod/goit-js-hw-09@2e7039b0584f5006847e51ca014620826372138c 🚀

---
+++ 01-color-switcher.a5e7de0a.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};t.startBtn.addEventListener("click",(function(){n=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.startBtn.disabled=!0})),t.stopBtn.addEventListener("click",(function(){clearInterval(n),t.startBtn.disabled=!1}));var n=null}();
+//# sourceMappingURL=01-color-switcher.a5e7de0a.js.map
