d271c3f1f45c9bbd051702ffe243ef6eb4a42c47
f0remangod <probill27@gmail.com>
2022-10-24 19:20:16 +0300

    timer

---
+++ src/js/02-timer.js
@@ -0,0 +1,76 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+const refs = {
+  datePicker: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+refs.startBtn.disabled = true;
+refs.startBtn.addEventListener('click', onStartClick);
+let ms = 0;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: Date.now(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0].getTime() <= Date.now()) {
+      Notify.failure('Are you mad?!');
+      return;
+    }
+    refs.startBtn.disabled = false;
+    selectedTime = selectedDates[0].getTime();
+  },
+};
+
+flatpickr(refs.datePicker, options);
+
+function onStartClick() {
+  const intervalId = setInterval(() => {
+    if (selectedTime < Date.now()) {
+      clearInterval(intervalId);
+      return;
+    }
+    ms = selectedTime - Date.now();
+    refs.startBtn.disabled = true;
+
+    console.log(convertMs(ms));
+    render(convertMs(ms));
+  }, 1000);
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+  //   console.log(convertMs(ms));
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function render({ days, hours, minutes, seconds }) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.minutes.textContent = `${minutes}`;
+  refs.seconds.textContent = `${seconds}`;
+}
